30e24c277914a891ad5590a0d8906fb935ca0c4e
edwincontreras <ehcontrerasg@gmail.com>
2026-06-19 15:19:02 -0500

    Se terminan los cambios para armonizar los estilos de acuerdo a los colores institucionales.

---
+++ js/i18n.js
@@ -0,0 +1,217 @@
+const translations = {
+  es: {
+    nav_home:       'Home',
+    nav_about:      'Nosotros',
+    nav_policies:   'Políticas',
+    nav_contact:    'Contacto',
+
+    hero_title:     'SOMOS',
+    hero_cycle:     'Agua|Energía|Iluminación',
+    hero_desc:      'Subsidiaria del grupo hídrico No. 1 en Italia, brindando servicios a 1.5 millones de habitantes en República Dominicana.',
+
+    services_title: 'Servicios',
+    svc_water:      'Agua',
+    svc_energy:     'Energía',
+    svc_light:      'Iluminación',
+
+    banner_title:   'Llevando vida en cada gota',
+    banner_desc:    'En Acea Dominicana nos especializamos en brindar servicios de gestión comercial para empresas prestadoras de servicios públicos.',
+
+    about_title:    'Nosotros',
+    mission_title:  'Misión',
+    mission_desc:   'Acea Dominicana está orientada al desarrollo de la gestión comercial de los servicios públicos y de ingeniería, para garantizar la satisfacción de nuestros clientes y el bienestar de la organización.',
+    vision_title:   'Visión',
+    vision_desc:    'Ser la mejor opción de la gestión comercial para empresas de servicios públicos en Latinoamérica.',
+    values_title:   'Valores',
+    values_desc:    'Compromiso, Trabajo en equipo, Excelencia, Enfoque al Cliente, Integridad e Innovación.',
+
+    policy_title:   'Políticas de Calidad',
+    policy_p1:      'En Acea Dominicana estamos comprometidos a ofrecer servicios de gestión comercial con los más altos estándares de calidad a empresas prestadoras de servicios públicos y a las partes interesadas, incorporando la prevención de los riesgos al asegurar el cumplimiento de sus requisitos y expectativas.',
+    policy_p2:      'Esto lo logramos garantizando la eficiencia de nuestros procesos, la creación de una cultura de servicio al cliente, la capacitación continua del personal, el cumplimiento de las leyes vigentes, el pensamiento basado en riesgo, la utilización de herramientas tecnológicas adecuadas y el mejoramiento continuo del sistema de gestión de calidad.',
+
+    contact_title:  'Contacto',
+    contact_addr:   'Autopista Las Américas, Esq. Calle Masonería. Ensanche Ozama, Santo Domingo. República Dominicana.',
+    contact_phone:  '(809) 222-9241',
+    contact_email:  'soporte@aceadominicana.com',
+
+    footer_rights:  'Acea Dominicana® 2026 | Todos los derechos reservados. |',
+    footer_privacy: 'Políticas de privacidad',
+    up_top:         'Ir arriba',
+
+    priv_title:  'Política de Privacidad',
+    priv_intro:  'La presente Política de Privacidad establece los términos en que Acea Dominicana S.A. usa y protege la información que es proporcionada por sus usuarios al momento de utilizar su sitio web. Esta compañía está comprometida con la seguridad de los datos de sus usuarios. Cuando le pedimos llenar los campos de información personal con la cual usted pueda ser identificado, lo hacemos asegurando que solo se empleará de acuerdo con los términos de este documento. Sin embargo, esta Política de Privacidad puede cambiar con el tiempo o ser actualizada, por lo que le recomendamos revisar continuamente esta página.',
+    priv_h1:     'Información que es recogida',
+    priv_p1:     'Nuestro sitio web podrá recoger información personal, por ejemplo: nombre, información de contacto como su dirección de correo electrónico e información demográfica. Asimismo, cuando sea necesario podrá ser requerida información específica para procesar algún pedido o realizar una entrega o facturación.',
+    priv_h2:     'Uso de la información recogida',
+    priv_p2a:    'Nuestro sitio web emplea la información con el fin de proporcionar el mejor servicio posible, particularmente para mantener un registro de usuarios, de pedidos en caso que aplique, y mejorar nuestros productos y servicios. Es posible que sean enviados correos electrónicos periódicamente con información que consideremos relevante para usted.',
+    priv_p2b:    'Acea Dominicana S.A. está altamente comprometida a mantener su información segura. Usamos los sistemas más avanzados y los actualizamos constantemente para asegurarnos de que no exista ningún acceso no autorizado.',
+    priv_h3:     'Cookies',
+    priv_p3a:    'Una cookie se refiere a un fichero que es enviado con la finalidad de solicitar permiso para almacenarse en su ordenador. Al aceptar dicho fichero, la cookie sirve para tener información respecto al tráfico web y facilita las futuras visitas a una web recurrente.',
+    priv_p3b:    'Nuestro sitio web emplea las cookies para poder identificar las páginas que son visitadas y su frecuencia. Esta información es empleada únicamente para análisis estadístico y posteriormente se elimina de forma permanente. Si se declinan las cookies, es posible que no pueda utilizar algunos de nuestros servicios.',
+    priv_h4:     'Enlaces a terceros',
+    priv_p4:     'Este sitio web pudiera contener enlaces a otros sitios que pudieran ser de su interés. Una vez que usted haga clic en estos enlaces y abandone nuestra página, ya no tenemos control sobre el sitio al que es redirigido y por lo tanto no somos responsables de los términos o privacidad ni de la protección de sus datos en esos otros sitios terceros.',
+    priv_h5:     'Control de su información personal',
+    priv_p5a:    'En cualquier momento usted puede restringir la recopilación o el uso de la información personal que es proporcionada a nuestro sitio web. En caso de que haya marcado la opción de recibir nuestro boletín o publicidad, usted puede cancelarla en cualquier momento.',
+    priv_p5b:    'Esta compañía no venderá, cederá ni distribuirá la información personal que es recopilada sin su consentimiento, salvo que sea requerido por un juez con una orden judicial.',
+    priv_reserve:'Acea Dominicana S.A. se reserva el derecho de cambiar los términos de la presente Política de Privacidad en cualquier momento.',
+  },
+
+  en: {
+    nav_home:       'Home',
+    nav_about:      'About Us',
+    nav_policies:   'Policies',
+    nav_contact:    'Contact',
+
+    hero_title:     'WE ARE',
+    hero_cycle:     'Water|Energy|Lighting',
+    hero_desc:      'Subsidiary of Italy\'s No. 1 water group, providing services to 1.5 million inhabitants in the Dominican Republic.',
+
+    services_title: 'Services',
+    svc_water:      'Water',
+    svc_energy:     'Energy',
+    svc_light:      'Lighting',
+
+    banner_title:   'Bringing life in every drop',
+    banner_desc:    'Acea Dominicana specializes in providing commercial management services for public utility companies.',
+
+    about_title:    'About Us',
+    mission_title:  'Mission',
+    mission_desc:   'Acea Dominicana is focused on developing commercial management of public utility and engineering services, ensuring client satisfaction and organizational well-being.',
+    vision_title:   'Vision',
+    vision_desc:    'To be the best commercial management option for public utility companies in Latin America.',
+    values_title:   'Values',
+    values_desc:    'Commitment, Teamwork, Excellence, Customer Focus, Integrity and Innovation.',
+
+    policy_title:   'Quality Policies',
+    policy_p1:      'At Acea Dominicana we are committed to providing commercial management services of the highest quality standards to public utility companies and stakeholders, incorporating risk prevention to ensure compliance with their requirements and expectations.',
+    policy_p2:      'We achieve this by guaranteeing the efficiency of our processes, creating a customer service culture, continuous staff training, compliance with applicable laws, risk-based thinking, the use of appropriate technological tools and continuous improvement of the quality management system.',
+
+    contact_title:  'Contact',
+    contact_addr:   'Autopista Las Américas, Esq. Calle Masonería. Ensanche Ozama, Santo Domingo. Dominican Republic.',
+    contact_phone:  '(809) 222-9241',
+    contact_email:  'soporte@aceadominicana.com',
+
+    footer_rights:  'Acea Dominicana® 2026 | All rights reserved. |',
+    footer_privacy: 'Privacy policy',
+    up_top:         'Go to top',
+
+    priv_title:  'Privacy Policy',
+    priv_intro:  'This Privacy Policy establishes the terms under which Acea Dominicana S.A. uses and protects the information provided by its users when using its website. This company is committed to the security of its users\' data. When we ask you to fill in personal information fields by which you can be identified, we do so ensuring it will only be used in accordance with the terms of this document. However, this Privacy Policy may change over time or be updated, so we recommend you review this page continuously.',
+    priv_h1:     'Information collected',
+    priv_p1:     'Our website may collect personal information such as: name, contact information including your email address, and demographic information. Likewise, when necessary, specific information may be required to process an order or make a delivery or billing.',
+    priv_h2:     'Use of collected information',
+    priv_p2a:    'Our website uses the information in order to provide the best possible service, particularly to maintain a record of users, orders where applicable, and to improve our products and services. Periodic emails may be sent through our site with information we consider relevant to you.',
+    priv_p2b:    'Acea Dominicana S.A. is highly committed to keeping your information secure. We use the most advanced systems and update them constantly to ensure there is no unauthorized access.',
+    priv_h3:     'Cookies',
+    priv_p3a:    'A cookie refers to a file that is sent to request permission to be stored on your computer. Upon accepting, the cookie is used to gather information about web traffic and facilitate future visits to a recurring website.',
+    priv_p3b:    'Our website uses cookies to identify which pages are visited and how frequently. This information is used only for statistical analysis and is then permanently deleted. If cookies are declined, you may not be able to use some of our services.',
+    priv_h4:     'Third-party links',
+    priv_p4:     'This website may contain links to other sites that may be of interest to you. Once you click on these links and leave our page, we no longer have control over the site to which you are redirected and therefore we are not responsible for the privacy terms or the protection of your data on those other third-party sites.',
+    priv_h5:     'Control of your personal information',
+    priv_p5a:    'At any time you may restrict the collection or use of personal information that is provided to our website. If you have opted to receive our newsletter or advertising, you can cancel it at any time.',
+    priv_p5b:    'This company will not sell, transfer or distribute the personal information that is collected without your consent, unless required by a judge with a court order.',
+    priv_reserve:'Acea Dominicana S.A. reserves the right to change the terms of this Privacy Policy at any time.',
+  },
+
+  it: {
+    nav_home:       'Home',
+    nav_about:      'Chi Siamo',
+    nav_policies:   'Politiche',
+    nav_contact:    'Contatti',
+
+    hero_title:     'SIAMO',
+    hero_cycle:     'Acqua|Energia|Illuminazione',
+    hero_desc:      'Filiale del gruppo idrico n. 1 in Italia, che fornisce servizi a 1,5 milioni di abitanti nella Repubblica Dominicana.',
+
+    services_title: 'Servizi',
+    svc_water:      'Acqua',
+    svc_energy:     'Energia',
+    svc_light:      'Illuminazione',
+
+    banner_title:   'Portando vita in ogni goccia',
+    banner_desc:    'Acea Dominicana è specializzata nell\'erogazione di servizi di gestione commerciale per aziende fornitrici di servizi pubblici.',
+
+    about_title:    'Chi Siamo',
+    mission_title:  'Missione',
+    mission_desc:   'Acea Dominicana è orientata allo sviluppo della gestione commerciale dei servizi pubblici e ingegneristici, per garantire la soddisfazione dei clienti e il benessere dell\'organizzazione.',
+    vision_title:   'Visione',
+    vision_desc:    'Essere la migliore opzione di gestione commerciale per le aziende di servizi pubblici in America Latina.',
+    values_title:   'Valori',
+    values_desc:    'Impegno, Lavoro di squadra, Eccellenza, Orientamento al cliente, Integrità e Innovazione.',
+
+    policy_title:   'Politiche di Qualità',
+    policy_p1:      'In Acea Dominicana siamo impegnati a offrire servizi di gestione commerciale con i più alti standard di qualità alle aziende fornitrici di servizi pubblici e alle parti interessate, incorporando la prevenzione dei rischi nel garantire la conformità ai loro requisiti e aspettative.',
+    policy_p2:      'Lo realizziamo garantendo l\'efficienza dei nostri processi, la creazione di una cultura del servizio al cliente, la formazione continua del personale, il rispetto delle leggi vigenti, il pensiero basato sul rischio, l\'utilizzo di strumenti tecnologici adeguati e il miglioramento continuo del sistema di gestione della qualità.',
+
+    contact_title:  'Contatti',
+    contact_addr:   'Autopista Las Américas, Esq. Calle Masonería. Ensanche Ozama, Santo Domingo. Repubblica Dominicana.',
+    contact_phone:  '(809) 222-9241',
+    contact_email:  'soporte@aceadominicana.com',
+
+    footer_rights:  'Acea Dominicana® 2026 | Tutti i diritti riservati. |',
+    footer_privacy: 'Informativa sulla privacy',
+    up_top:         'Torna su',
+
+    priv_title:  'Informativa sulla Privacy',
+    priv_intro:  'La presente Informativa sulla Privacy stabilisce i termini in cui Acea Dominicana S.A. utilizza e protegge le informazioni fornite dai propri utenti al momento dell\'utilizzo del sito web. Questa azienda è impegnata nella sicurezza dei dati dei propri utenti. Quando ti chiediamo di compilare i campi delle informazioni personali con cui puoi essere identificato, lo facciamo assicurandoci che vengano utilizzate solo in conformità con i termini di questo documento.',
+    priv_h1:     'Informazioni raccolte',
+    priv_p1:     'Il nostro sito web potrà raccogliere informazioni personali come: nome, informazioni di contatto tra cui il tuo indirizzo email e informazioni demografiche. Quando necessario, potranno essere richieste informazioni specifiche per elaborare un ordine o effettuare una consegna o fatturazione.',
+    priv_h2:     'Utilizzo delle informazioni raccolte',
+    priv_p2a:    'Il nostro sito web utilizza le informazioni al fine di fornire il miglior servizio possibile, in particolare per mantenere un registro degli utenti, degli ordini ove applicabile, e per migliorare i nostri prodotti e servizi. È possibile che vengano inviati periodicamente messaggi email con informazioni che riteniamo rilevanti per l\'utente.',
+    priv_p2b:    'Acea Dominicana S.A. è fortemente impegnata a mantenere le tue informazioni al sicuro. Utilizziamo i sistemi più avanzati e li aggiorniamo costantemente per assicurarci che non vi siano accessi non autorizzati.',
+    priv_h3:     'Cookie',
+    priv_p3a:    'Un cookie si riferisce a un file inviato con la finalità di richiedere il permesso di essere memorizzato sul tuo computer. Accettando tale file, il cookie serve a raccogliere informazioni sul traffico web e facilita le future visite a un sito web ricorrente.',
+    priv_p3b:    'Il nostro sito web utilizza i cookie per identificare le pagine visitate e la loro frequenza. Queste informazioni vengono utilizzate esclusivamente per analisi statistiche e successivamente eliminate definitivamente. Se i cookie vengono rifiutati, potrebbe non essere possibile utilizzare alcuni dei nostri servizi.',
+    priv_h4:     'Link a terzi',
+    priv_p4:     'Questo sito web potrebbe contenere link ad altri siti che potrebbero essere di tuo interesse. Una volta che fai clic su questi link e lasci la nostra pagina, non abbiamo più controllo sul sito a cui sei reindirizzato e pertanto non siamo responsabili dei termini di privacy né della protezione dei tuoi dati su quei siti terzi.',
+    priv_h5:     'Controllo delle informazioni personali',
+    priv_p5a:    'In qualsiasi momento puoi limitare la raccolta o l\'uso delle informazioni personali fornite al nostro sito web. Se hai scelto di ricevere la nostra newsletter o pubblicità, puoi annullare l\'iscrizione in qualsiasi momento.',
+    priv_p5b:    'Questa azienda non venderà, cederà né distribuirà le informazioni personali raccolte senza il tuo consenso, salvo che sia richiesto da un giudice con un ordine del tribunale.',
+    priv_reserve:'Acea Dominicana S.A. si riserva il diritto di modificare i termini della presente Informativa sulla Privacy in qualsiasi momento.',
+  }
+};
+
+function applyLanguage(lang) {
+  const t = translations[lang];
+  if (!t) return;
+
+  document.querySelectorAll('[data-i18n]').forEach(function(el) {
+    const key = el.getAttribute('data-i18n');
+    if (t[key] !== undefined) {
+      if (el.tagName === 'INPUT' || el.tagName === 'TEXTAREA') {
+        el.placeholder = t[key];
+      } else {
+        el.textContent = t[key];
+      }
+    }
+  });
+
+  // Texto del ciclo animado
+  var cycleEl = document.getElementById('text-animation');
+  if (cycleEl && t.hero_cycle) {
+    cycleEl.textContent = t.hero_cycle;
+    if (typeof $.fn.cycleText === 'function') {
+      $(cycleEl).cycleText();
+    }
+  }
+
+  // Actualiza atributos de lang
+  document.documentElement.lang = lang;
+  localStorage.setItem('aceaLang', lang);
+
+  // Marca el botón activo
+  document.querySelectorAll('.lang-btn').forEach(function(btn) {
+    btn.classList.toggle('active', btn.getAttribute('data-lang') === lang);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var saved = localStorage.getItem('aceaLang') || 'es';
+  applyLanguage(saved);
+
+  document.querySelectorAll('.lang-btn').forEach(function(btn) {
+    btn.addEventListener('click', function() {
+      applyLanguage(this.getAttribute('data-lang'));
+    });
+  });
+});
